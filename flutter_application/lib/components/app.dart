e8174536fa03de3b7eebaa8c63be4c6640366981
ngocphucnguyen256 <69982260+ngocphucnguyen256@users.noreply.github.com>
2021-12-07 10:51:45 +0700

    desktop nav

--- flutter_application/lib/components/app.dart
+++ flutter_application/lib/components/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/components/menu.dart';
 import 'package:flutter_application/main.dart';
 
 class App extends StatelessWidget {
@@ -6,46 +7,35 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Container(
-            color: subBgColor,
-            child: Column(children: <Widget>[
-              Expanded(
-                  flex: 3,
-                  child: Container(
-                    margin: EdgeInsets.all(20),
-                    width: double.infinity,
-                    height: 100.0,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      color: Colors.blue,
-                    ),
-                    child: Text('Dashboard'),
-                  )),
-            ]),
+    var flex2 = 1.5;
+    return Container(
+      color: bgColor,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Menu(),
           ),
-        ),
-        Expanded(
-          flex: 4,
-          child: Container(
-            color: Colors.red,
-            child: Center(
-              child: Text('Hello World'),
+          Expanded(
+            flex: 6,
+            child: Container(
+              color: Colors.red,
+              child: Center(
+                child: Text('Hello World'),
+              ),
             ),
           ),
-        ),
-        Expanded(
-          flex: 3,
-          child: Container(
-            color: Colors.green,
-            child: Center(
-              child: Text('Hello World'),
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: Colors.green,
+              child: Center(
+                child: Text('Hello World'),
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- flutter_application/lib/components/menu.dart
+++ flutter_application/lib/components/menu.dart
@@ -1,13 +1,134 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/main.dart';
 
 class Menu extends StatelessWidget {
   const Menu({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(),
+    return Container(
+      margin: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: subBgColor,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    MaterialButton(
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.home,
+                              color: Colors.white,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              'Dashboard',
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      hoverColor: Colors.blue,
+                    ),
+                    MaterialButton(
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.add_circle, color: Colors.white),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              'Transactions',
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      hoverColor: Colors.blue,
+                    ),
+                    MaterialButton(
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.help, color: Colors.white),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              'Help',
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      hoverColor: Colors.blue,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                child: MaterialButton(
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.logout,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Logout',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  hoverColor: Colors.blue,
+                ),
+              ),
+            ]),
+      ),
     );
   }
 }
